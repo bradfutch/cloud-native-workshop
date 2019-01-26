b77d58657943df4b4a1b8634fdd222e767e73805
Brad Futch <bradfutch@gmail.com>
2019-01-26 12:19:25 -0800

    Circuit Breaker Complete

--- labs/my_work/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
+++ labs/my_work/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
@@ -2,6 +2,7 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class CloudNativeSpringUiApplication {
 
