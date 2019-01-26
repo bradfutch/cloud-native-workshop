a29966798e2dbdcc6fa0a951c6bd95860c674f88
Brad Futch <bradfutch@gmail.com>
2019-01-26 11:50:05 -0800

    Config Server integration complete

--- labs/my_work/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
+++ labs/my_work/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
@@ -1,14 +1,20 @@
 package io.pivotal.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class GreetingController {
 
+    @Value("${greeting:Hola}")
+    private String greeting;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello PIMCO!";
+        return greeting + " PIMCO!";
     }
 
 }
