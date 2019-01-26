0c1ade30e53c2a71133b7e5b29f8cbf21afee6a8
Brad Futch <bradfutch@gmail.com>
2019-01-26 12:06:08 -0800

    Service Registry Complete

--- labs/my_work/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
+++ labs/my_work/cloud-native-spring-ui/src/main/java/io/pivotal/CloudNativeSpringUiApplication.java
@@ -2,12 +2,32 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CloudNativeSpringUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudNativeSpringUiApplication.class, args);
 	}
+
+    @Order(101)
+    @Configuration
+    static class ApplicationSecurityOverride extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web
+                    .ignoring()
+                    .antMatchers("/**");
+        }
+    }
 	
 }
